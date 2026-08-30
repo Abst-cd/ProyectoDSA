83f36c2b9e12c8dc92ab05e746fc96967258e2f5
Abstract <pacodejesus2006@gmail.com>
2026-08-29 21:59:19 -0600

    added a linked list for the proyect

---
+++ LinkedList.java
@@ -0,0 +1,90 @@
+    //re utilizacion de codigo de la actividad 1
+    public class LinkedList<T>{
+    private Node<T> head;
+    private Node<T> tail;
+
+
+    public void insert(T d){
+      Node<T> nuevoNodo = new Node<>(d, head, 0);
+
+      if(head == null){
+          head = nuevoNodo;
+          tail = nuevoNodo;
+      } else {
+        Node<T> current = head;
+        while(current.getNext() != null){
+            current = current.getNext();
+        }
+        current.setNext(nuevoNodo);
+
+        tail = nuevoNodo;
+      }
+    }
+
+        public int size(){
+         int size = 0;
+         Node<T> actual = head;
+
+            if (actual==null){
+                return 0;
+            } else {
+                
+                while(actual!=null){
+                    actual = actual.getNext();
+                    size++;
+                }    
+            } return size;
+         
+        } 
+
+        public T delete(){
+
+            if (head == null){
+                return null;
+            }
+           T valoraEliminar = head.getData();
+           head = head.getNext();
+           return valoraEliminar;
+        }
+
+    public void display(){
+        Node<T> current = head;
+        while(current != null){
+            System.out.println(current.getData());
+            current = current.getNext();
+        }
+    }
+
+    public T getHead(){
+        if(this.head != null){
+            return this.head.getData();
+        }
+        return null;
+    }
+
+    public Node<T> getNodeHead(){
+        return head;
+    }
+
+     public T getTail(){
+        if (this.tail !=null){
+            return this.tail.getData();
+        }
+        return null;
+    }
+
+@Override
+public String toString() {
+    String resultado = "";
+
+    Node<T> current = head;
+
+    while (current != null) {
+        resultado += current.getData() + " ";
+        current = current.getNext();
+    }
+
+    return resultado;
+}
+
+}
